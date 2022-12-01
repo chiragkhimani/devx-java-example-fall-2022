f0fb6d7b047bc15467810d48b719745c0f439509
chiragkhimani <khimanichirag@gmail.com>
2022-12-01 17:25:37 +0530

    Class 33

---
+++ src/com/java/class33/RandomNumberGenerator.java
@@ -0,0 +1,10 @@
+package com.java.class33;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(74));
+    }
+}
